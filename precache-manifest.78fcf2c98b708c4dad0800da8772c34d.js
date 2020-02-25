15136e60314ce79762b94e990508212cb22d69f5
rawdEel <claeswdev@gmail.com>
2020-02-25 13:51:51 +0100

    Updates

---
+++ precache-manifest.78fcf2c98b708c4dad0800da8772c34d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f149e2953893c7ed5fb421e03a325cab",
+    "url": "/claesdev/react-tutorial/index.html"
+  },
+  {
+    "revision": "156d32ed392aacf7f61f",
+    "url": "/claesdev/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "6edcdf994bf5c4113157",
+    "url": "/claesdev/react-tutorial/static/js/2.1ceb358c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/claesdev/react-tutorial/static/js/2.1ceb358c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156d32ed392aacf7f61f",
+    "url": "/claesdev/react-tutorial/static/js/main.6212661f.chunk.js"
+  },
+  {
+    "revision": "07215d335043e7ba1458",
+    "url": "/claesdev/react-tutorial/static/js/runtime-main.a5f4d048.js"
+  }
+]);
